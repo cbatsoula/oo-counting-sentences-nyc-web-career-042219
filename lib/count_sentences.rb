fefcb4ceb09b48edb144bf7d5eb82b0df236cf07
Catherine Batsoula <cbatsoula@gmail.com>
2019-04-23 20:31:33 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,7 +15,7 @@ class String
   end
 
   def count_sentences
-    self.split(/\.|\?|\!/).size.delete_if { |word| word.size < 2}
-    binding.pry
+    self.split(/\.|\?|\!/).delete_if { |w| w.size < 2 }.size
+    
   end
 end
